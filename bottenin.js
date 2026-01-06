fd6e1967046f554506a3c217cf15fc36b60e5f9f
Ghanshyam <gc.cse28@gmail.com>
2026-01-06 09:15:21 +0530

    Add files via upload
    this is a  button  web page . In this have one html button and using same js

---
+++ bottenin.js
@@ -0,0 +1,15 @@
+let modeBotn = document.querySelector("#mode");
+let currmode ="light";
+
+ 
+modeBotn.addEventListener("click", () => {
+    if(currmode =="light"){
+        currmode="dark";
+        document.querySelector("body").style.backgroundColor="black";
+    }
+    else{
+        currmode="light";
+        document.querySelector("body").style.backgroundColor="white";
+    }
+    console.log(currmode);
+})
